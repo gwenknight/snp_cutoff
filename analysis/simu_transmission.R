4c9a0e5084110e5d7ffa72c8d1fd64f771a7c470
GwenKn <g.knight@imperial.ac.uk>
2018-02-26 15:27:10 +0000

    moved code to function .R file add in constant vs. changing sample times and mutation rate

--- analysis/simu_transmission.R
+++ analysis/simu_transmission.R
@@ -4,32 +4,20 @@
 library(scales)
 library(truncnorm)
 library(reshape2)
+library(ggplot2)
 
 ### Home
 home <- "~/Documents/snp_cutoff/"
 plots <- "~/Documents/plots"
 theme_set(theme_bw(base_size = 24))
 
-###*** Functions ***############################################################################################################
-# Sample from own distribution
-rMydist <- function(n,x) {
-  sample(seq(1,length(x),1), size = n, prob = x, replace=T)
-}
-# Sample from distribution 
-rMydist_plus <- function(n,x,n_mut) {
-  sample(seq(1,length(x)+n_mut,1), size = n, prob = c(matrix(0,1,n_mut),x), replace=T) # zero probability of less than n_muts
-}
-## General model for how distance from transmitted changes over time
-gen_mod_day <- function(day,l_x){
-  x <- seq(0,l_x,1)
-  aa_g <-  0.99338 #mod_a_mod[1]
-  bb_g <- -0.02207 #mod_a_mod[2]
-  out <- exp(exp(aa_g + bb_g*day) + p_aa * x)
-  return(out)
-}
+###*** Functions ***############################################################################################################################################################
+setwd(home)
+source("analysis/simu_transmission_fn.R")
 
 ###*** Times ***############################################################################################################################################################
-times <- read.csv("../data/sample_time_dist2.csv")[,-1]
+times <- read.csv("../data/sample_time_dist2.csv")[,-1] ### t distribution from FC
+times_c <- 180 # 6 months to match FC
 
 ###*** mutation rate ***###################################################################################################################################################
 mu = 5 / 365 # 5 SNPS per year
@@ -41,57 +29,32 @@ mu_h = 6.37 / 365
 mn = mu_l + (mu_h - mu_l)/2
 sdmn = mn/3
 
-#### RUNS
-store_all <- c()
-store_limits <- c()
+###*** RUNS ***###################################################################################################################################################
+setwd("plots")
+npat = 1000
+nruns = 1000
 
-for(j in 1:1000){
-  
-  ### STORE / PREP
-  n = 1000 # number of pairs of samples
-  
-  store <- matrix(0,n,2)
-  
-  # SAMPLE mutation rates to use
-  mu_v <- rtruncnorm(n,a = 0, b = Inf, mn,sdmn) # lower truncated to be positive
-  
-  # SAMPLE times to use
-  ht <- hist(times,breaks = seq(0,500,1),plot = FALSE)
-  times_v <- rMydist(n,ht$counts) 
-  
-  for(i in 1:n){
-    sample_gap <- times_v[i]
-    store[i,1] <- sample_gap # store time
-    
-    # SOURCE
-    dis_source <- gen_mod_day(sample_gap,150) 
-    dis_source_from_transm <- rMydist(1,dis_source)
-    
-    # RECEIVER
-    mut_number <- rpois(1,mu_v[i] * sample_gap) # number of mutations likely to have occured since sample
-    dis_receiver_from_transm <- rexp(1,1/mut_number) # 1/mean (rate in this definition of exp)
-    
-    # DISTANCE SOURCE <- TRANS -> RECEIVER
-    mut_dist <- dis_source_from_transm + dis_receiver_from_transm
-    store[i,2] <- mut_dist # store mut distance
-  }
-  
-  #plot(store[,1], store[,2], xlab = "Time Distance (in days)", ylab = "SNP distance")
-  h<-hist(store[,2],breaks = seq(1,400,1),plot=FALSE)
-  h_cum <- cumsum(h$density)
-  #plot(seq(1,39,1),h_cum)
-  l1<-length(which(h_cum < 0.7)) # 70% 1 SNP differences
-  l2<-length(which(h_cum < 0.9)) # 90% 4 SNPS different
-  l3<-length(which(h_cum < 0.95)) # 95% 6 SNPS different 
-  l4<-length(which(h_cum < 0.99)) # 99% 14 SNPS different
-  
-  ### Store
-  store_all <- rbind(store_all,cbind(j,store))
-  store_limits <- rbind(store_limits,c(j,l1,l2,l3,l4))
-  
-}
+###*** CONSTANT ***###################################################################################################################################################
+s_constant <- simu_runs(times_c, mu, npat, nruns,0)
+p <- ggplot(s_constant$store,aes(x=value)) + geom_histogram(aes(y = ..density..),binwidth = 1) + 
+  facet_wrap(~variable) + scale_x_continuous("SNP distance") + scale_y_continuous("Probability")
+ggsave("constant_SNP_dist_limits.pdf")
 
-store_limits <- as.data.frame(store_limits)
-colnames(store_limits) <- c("run","l70","l90","l95","l99")
-m_store_limits <- melt(store_limits,id.vars = "run")
-ggplot(m_store_limits,aes(x=value)) + geom_histogram(stat="count") + facet_wrap(~variable)
+###*** VARYING TIMES ***###################################################################################################################################################
+s_varying <- simu_runs(times, mu, npat, nruns,0)
+p <- ggplot(s_varying$store,aes(x=value)) + geom_histogram(aes(y = ..density..),binwidth = 1) + 
+  facet_wrap(~variable) + scale_x_continuous("SNP distance") + scale_y_continuous("Probability")
+ggsave("varying_SNP_dist_limits.pdf")
+
+###*** VARYING TIMES & MU ***###################################################################################################################################################
+s_varyingtmu <- simu_runs(times, c(mn,sdmn), npat, nruns,0)
+p <- ggplot(s_varyingtmu$store,aes(x=value)) + geom_histogram(aes(y = ..density..),binwidth = 1) + 
+  facet_wrap(~variable) + scale_x_continuous("SNP distance") + scale_y_continuous("Probability")
+ggsave("varyingtmu_SNP_dist_limits.pdf")
+
+###*** CONSTANT & SAME MODEL FOR SOURCE/RECIPIENT ***###################################################################################################################################################
+s_constant_same <- simu_runs(times_c, mu, npat, nruns,1)
+p <- ggplot(s_constant_same$store,aes(x=value)) + geom_histogram(aes(y = ..density..),binwidth = 1) + 
+  facet_wrap(~variable) + scale_x_continuous("SNP distance") + scale_y_continuous("Probability")
+ggsave("constant_same_SNP_dist_limits.pdf")
+
