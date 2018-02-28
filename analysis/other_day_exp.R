d6fb8b115586ba371f4fc1f42c99e50f7709d212
GwenKn <g.knight@imperial.ac.uk>
2018-02-28 14:07:42 +0000

    stored plots

--- analysis/other_day_exp.R
+++ analysis/other_day_exp.R
@@ -4,6 +4,18 @@ dd <- read.csv("~/Documents/snp_cutoff/data/mrsa_pros.csv")
 u<-sort(unique(dd$liDayGap))
 coef_store<-c(); data_store<-c()
 
+# trial
+i = 23
+w<-which(dd$liDayGap == u[i])
+h <- hist(dd[w,]$liSNPdis,breaks=seq(0,max(dd[w,"liSNPdis"]),1),plot = TRUE)
+ggplot(dd[w,],aes(x=liSNPdis)) + geom_histogram(stat="count") + scale_x_continuous("SNP distance") + scale_y_continuous("Count")
+ggsave("day_23_ggplot.pdf")
+data <- data.frame( x = h$breaks[-length(h$breaks)], y = h$counts)
+pdf("day_23.pdf")
+plot(data$x,data$y)
+lines(data$x,exp(1.665 + -0.976 * data$x), col="red")
+dev.off()
+
 for(i in 1:length(u)){
   w<-which(dd$liDayGap == u[i])
   if(length(w)>10){ # need > 10? for fit to be ok
@@ -35,16 +47,25 @@ mod_a <- nls(a ~ exp(aa + bb * day), data = coef_store, start = list(aa = 1.4, b
 lines(coef_store$day, predict(mod_a, list(x = coef_store$day)),col="red")
 mod_a_mod <- coef(mod_a)
 
-plot(coef_store[,1],coef_store[,3])
+pdf("a_exponential.pdf")
+plot(coef_store$day,coef_store$a,ylim = c(0,4),xlab = "Time between samples",ylab="Intercept") 
+lines(coef_store$day, predict(mod_a, list(x = coef_store$day)),col="red")
+dev.off()
+
+pdf("b_flat.pdf")
+plot(coef_store[,1],coef_store[,3],xlab = "Time between samples",ylab = "Slope")
 # Assume all v similar - take mean
 mean(coef_store[,3])
 p_aa <- mean(coef_store[which(coef_store[,3] > (-8)),3]) # remove outliers
+abline(h = p_aa,col="red",lty="dashed")
+dev.off()
 
 
 data_store$mod_general <- exp(exp(mod_a_mod[1] + mod_a_mod[2]*data_store[,"day"]) + p_aa * data_store[,"x"])
   
 ggplot(data_store,aes(x=x, y=y)) + geom_point() + geom_line(aes(x=x,y=m),col="red") + 
-  facet_wrap(~day,scales="free") + geom_line(aes(x=x,y=mod_general),col="blue")
+  facet_wrap(~day,scales="free") + geom_line(aes(x=x,y=mod_general),col="blue") + 
+   scale_x_continuous("SNP distance") + scale_y_continuous("Count")
 ggsave("exponential_other_day.pdf",width = 12, height = 12)
 
 ## General model 
